2d4f501784c3efb353785704a17dc2000a6030c5
radness <yhs5128@gmail.com>
2024-07-17 11:12:34 +0900

    apply springdoc swagger ui

---
+++ src/main/java/com/radness/sf/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.radness.sf.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨.
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                // stateless한 rest api를 개발할 것이므로 csrf 공격에 대한 옵션은 꺼둔다.
+                .csrf(AbstractHttpConfigurer::disable)
+                // 특정 URL에 대한 권한 설정.
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                new AntPathRequestMatcher("/"),
+                                new AntPathRequestMatcher("/swagger-ui/**"),
+                                new AntPathRequestMatcher("/api-docs/**"),
+                                new AntPathRequestMatcher("/css/**"),
+                                new AntPathRequestMatcher("/images/**"),
+                                new AntPathRequestMatcher("/js/**"),
+                                new AntPathRequestMatcher("/h2-console/**"),
+                                new AntPathRequestMatcher("/profile")
+                        ).permitAll()
+                        .anyRequest().authenticated())
+//                .formLogin((formLogin) -> {
+//                    /* 권한이 필요한 요청은 해당 url로 리다이렉트 */
+//                    formLogin.loginPage("/login");
+//                })
+                .logout((logout) -> logout
+                        .logoutSuccessUrl("/"))
+                .build();
+    }
+}

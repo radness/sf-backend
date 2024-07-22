8814fdca19d291a72d85ad4ae0f4f2a1f07514e8
radness <yhs5128@gmail.com>
2024-07-22 23:40:36 +0900

    wip lot

--- src/main/java/com/radness/sf/wip/lot/history/WipLotHistoryRepository.java
+++ src/main/java/com/radness/sf/wip/lot/history/WipLotHistoryRepository.java
@@ -2,5 +2,6 @@ package com.radness.sf.wip.lot.history;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface WipLotHistoryRepository extends JpaRepository<WipLotHistory, Long> {
 
 }

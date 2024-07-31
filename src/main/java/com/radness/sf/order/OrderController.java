600da1a164021908f48bfac80579ed85131af693
radness <yhs5128@gmail.com>
2024-07-31 11:37:18 +0900

    modify order

--- src/main/java/com/radness/sf/order/OrderController.java
+++ src/main/java/com/radness/sf/order/OrderController.java
@@ -3,6 +3,7 @@ package com.radness.sf.order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -13,7 +14,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @GetMapping("/{orderId}")
-    public Optional<Order> viewOrder(@PathVariable("orderId") String orderId) {
+    public Optional<Order> getOrder(@PathVariable("orderId") String orderId) {
         return orderService.getOrder(orderId);
     }
 
@@ -22,8 +23,59 @@ public class OrderController {
         return orderService.createOrder(order);
     }
 
+    @PutMapping("/{orderId}")
+    public Order updateOrder(@PathVariable("orderId") String orderId, @RequestBody Order order) {
+        return orderService.updateOrder(orderId, order);
+    }
+
     @DeleteMapping("/{orderId}")
     public void deleteOrder(@PathVariable("orderId") String orderId) {
         orderService.deleteOrder(orderId);
     }
+
+    @GetMapping()
+    public List<Order> getOrders(Order order) {
+        return orderService.getOrders(order);
+    }
+
+    @PostMapping("/create")
+    public void createOrders(@RequestBody List<Order> orders) {
+        orderService.createOrders(orders);
+    }
+
+    @PostMapping("/save")
+    public void saveOrders(@RequestBody List<Order> orders) {
+        orderService.saveOrders(orders);
+    }
+
+    @DeleteMapping()
+    public void deleteOrders(@RequestBody List<Order> orders) {
+        orderService.deleteOrders(orders);
+    }
+
+    @PostMapping("/confirm")
+    public void confirmOrders(@RequestBody List<Order> orders) {
+        orderService.confirmOrders(orders);
+    }
+
+    @PostMapping("/cancel")
+    public void cancelOrders(@RequestBody List<Order> orders) {
+        orderService.cancelOrders(orders);
+    }
+
+    @PostMapping("/process")
+    public void processOrders(@RequestBody List<Order> orders) {
+        orderService.processOrders(orders);
+    }
+
+    @PostMapping("/wait")
+    public void waitOrders(@RequestBody List<Order> orders) {
+        orderService.waitOrders(orders);
+    }
+
+    @PostMapping("/end")
+    public void endOrders(@RequestBody List<Order> orders) {
+        orderService.endOrders(orders);
+    }
+
 }
--- src/main/java/com/radness/sf/order/OrderService.java
+++ src/main/java/com/radness/sf/order/OrderService.java
@@ -3,6 +3,7 @@ package com.radness.sf.order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,4 +23,45 @@ public class OrderService {
     public void deleteOrder(String orderId) {
         orderRepository.deleteById(orderId);
     }
+
+    public Order updateOrder(String orderId, Order order) {
+        return null;
+    }
+
+    public List<Order> getOrders(Order order) {
+        return null;
+    }
+
+    public void createOrders(List<Order> orders) {
+    }
+
+    public void saveOrders(List<Order> orders) {
+
+    }
+
+    public void deleteOrders(List<Order> orders) {
+
+    }
+
+    public void confirmOrders(List<Order> orders) {
+
+    }
+
+    public void cancelOrders(List<Order> orders) {
+
+    }
+
+    public void processOrders(List<Order> orders) {
+
+    }
+
+    public void waitOrders(List<Order> orders) {
+
+    }
+
+    public void endOrders(List<Order> orders) {
+
+    }
+
+
 }

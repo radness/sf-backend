732636b1fdbf3c5634650f56f0f8a22afee05d1a
radness <yhs5128@gmail.com>
2024-07-31 11:17:13 +0900

    modify wip lot controller

--- src/main/java/com/radness/sf/wip/lot/WipLotController.java
+++ src/main/java/com/radness/sf/wip/lot/WipLotController.java
@@ -1,8 +1,10 @@
 package com.radness.sf.wip.lot;
 
+import com.radness.sf.wip.lot.history.WipLotHistory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,9 +19,29 @@ public class WipLotController {
         return wipLotService.getWipLot(lotId);
     }
 
-    @PostMapping()
-    public WipLot createWipLot(@RequestBody WipLot wipLot) {
-        return wipLotService.createWipLot(wipLot);
+    @GetMapping()
+    public Optional<List<WipLot>> viewWipLotList(@RequestParam String lotId) {
+        return wipLotService.getWipLotList(lotId);
+    }
+
+    @GetMapping("/{lotId}/history")
+    public Optional<List<WipLotHistory>> viewWipLotHistory(@PathVariable String lotId) {
+        return wipLotService.getWipLotHistory(lotId);
+    }
+
+    @PostMapping("/{lotId}/create")
+    public WipLot createWipLot(@PathVariable String lotId, @RequestBody WipLot wipLot) {
+        return wipLotService.createWipLot(lotId, wipLot);
+    }
+
+    @PostMapping("/{lotId}/start")
+    public WipLot startWipLot(@PathVariable String lotId, @RequestBody WipLot wipLot) {
+        return wipLotService.startWipLot(lotId, wipLot);
+    }
+
+    @PostMapping("/{lotId}/end")
+    public WipLot endWipLot(@PathVariable String lotId, @RequestBody WipLot wipLot) {
+        return wipLotService.endWipLot(lotId, wipLot);
     }
 
     @DeleteMapping("/{lotId}")
@@ -27,7 +49,49 @@ public class WipLotController {
         wipLotService.deleteWipLot(lotId);
     }
 
-    public void startWipLot(@PathVariable String lotId) {
-
+    @PostMapping("/{lotId}/pull")
+    public WipLot pullWipLot(@PathVariable String lotId, @RequestBody WipLot wipLot) {
+        return wipLotService.pullWipLot(lotId, wipLot);
     }
+
+    @PostMapping("/{lotId}/move")
+    public WipLot moveWipLot(@PathVariable String lotId, @RequestBody WipLot wipLot) {
+        return wipLotService.moveWipLot(lotId, wipLot);
+    }
+
+    @PostMapping("/{lotId}/split")
+    public WipLot splitWipLot(@PathVariable String lotId, @RequestBody WipLot wipLot) {
+        return wipLotService.splitWipLot(lotId, wipLot);
+    }
+
+    @PostMapping("/{lotId}/merge")
+    public WipLot mergeWipLot(@PathVariable String lotId, @RequestBody WipLot wipLot) {
+        return wipLotService.mergeWipLot(lotId, wipLot);
+    }
+
+    @PostMapping("/{lotId}/hold")
+    public WipLot holdWipLot(@PathVariable String lotId, @RequestBody WipLot wipLot) {
+        return wipLotService.holdWipLot(lotId, wipLot);
+    }
+
+    @PostMapping("/{lotId}/release")
+    public WipLot releaseWipLot(@PathVariable String lotId, @RequestBody WipLot wipLot) {
+        return wipLotService.releaseWipLot(lotId, wipLot);
+    }
+
+    @PostMapping("/{lotId}/rework")
+    public WipLot reworkWipLot(@PathVariable String lotId, @RequestBody WipLot wipLot) {
+        return wipLotService.reworkWipLot(lotId, wipLot);
+    }
+
+    @PostMapping("/{lotId}/loss")
+    public WipLot lossWipLot(@PathVariable String lotId, @RequestBody WipLot wipLot) {
+        return wipLotService.lossWipLot(lotId, wipLot);
+    }
+
+    @PostMapping("/{lotId}/correction")
+    public WipLot correctionWipLot(@PathVariable String lotId, @RequestBody WipLot wipLot) {
+        return wipLotService.correctionWipLot(lotId, wipLot);
+    }
+
 }
